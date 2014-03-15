2103a4f926809495221d291de10efee307e85092
kyle kersey <kylelk@comcast.net>
2014-03-14 19:23:43 -0700

    added files

--- update.sh
+++ update.sh
@@ -1,1 +1,1 @@
-rm code/* ; time python update.py ; cd code; tree -L 1 -HCh . -T "site map" -o index.html ; cd .. ; tree -HCh . -T "site map" -o sitemap.html ; python ~/insert_tracking.py
+rm code/* ; time python update.py ; cd code; tree -HCh . -T "site map" -o index.html ; cd .. ; tree -HCh . -T "site map" -o sitemap.html ; python ~/insert_tracking.py
